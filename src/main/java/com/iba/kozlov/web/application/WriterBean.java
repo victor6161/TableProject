dca284d0cd7059f0f3e2bded2ef9d9b7590220f8
Viktar Kazlou <KazlouV@iba>
2017-03-02 12:59:49 +0300

    fixed bug  validations error in oneMenu writer

--- src/main/java/com/iba/kozlov/web/application/WriterBean.java
+++ src/main/java/com/iba/kozlov/web/application/WriterBean.java
@@ -5,6 +5,44 @@ import lombok.Setter;
 
 public class WriterBean {
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WriterBean other = (WriterBean) obj;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+	@Override
 	public String toString() {
 		return "WriterBean [id=" + id + ", name=" + name + ", surname=" + surname + "]";
 	}
