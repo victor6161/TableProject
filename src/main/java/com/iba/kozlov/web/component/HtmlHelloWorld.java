1236088580816e865d8ed128b9f2e98e368967bb
Viktar Kazlou <KazlouV@iba>
2017-03-20 14:28:21 +0300

    added test chart from component

--- src/main/java/com/iba/kozlov/web/component/HtmlHelloWorld.java
+++ src/main/java/com/iba/kozlov/web/component/HtmlHelloWorld.java
@@ -1,6 +1,8 @@
 package com.iba.kozlov.web.component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponentBase;
@@ -9,11 +11,10 @@ import javax.faces.context.ResponseWriter;
 
 import org.apache.log4j.Logger;
 
-
-
 @FacesComponent(value = "HtmlHelloWorld")
 public class HtmlHelloWorld extends UIComponentBase {
-	private static final Logger LOGGER = Logger.getLogger(HtmlHelloWorld .class);
+	private static final Logger LOGGER = Logger.getLogger(HtmlHelloWorld.class);
+
 	@Override
 	public String getFamily() {
 		LOGGER.info("getFamily");
@@ -23,40 +24,64 @@ public class HtmlHelloWorld extends UIComponentBase {
 	@Override
 	public void encodeBegin(FacesContext context) throws IOException {
 		LOGGER.info("encodeAll");
-		ResponseWriter writer = context.getResponseWriter();
+
+		//создать класс со значениями авторов
 		
+		ResponseWriter writer = context.getResponseWriter();
 		writer.write("Hello World!");
-		StringBuilder html=new StringBuilder();
+		StringBuffer json = new StringBuffer();
+
+		json.append("\nfunction exampleData() {\n");
+		json.append("return  [\n");
+		json.append("{\n");
+		json.append("key: 'Cumulative Return',\n");
+		json.append("values: [\n");
+		json.append("{ \n");
+		json.append("'label' : 'A Label' ,\n");
+		json.append("'value' : -29.765957771107\n");
+		json.append(" } , \n");
+		json.append(" { \n");
+		json.append("  'label' : 'B Label' ,  \n");
+		json.append("'value' : 21\n");
+		json.append(" }  \n");
+		json.append(" ]  \n");
+		json.append(" }  \n");
+		json.append(" ]  \n"); 
+		json.append(" }  \n");
+
+		StringBuffer html = new StringBuffer();
 		html.append("<div id='chart'>");
 		html.append("<svg></svg>");
 		html.append("</div>");
 		
+
 		writer.write(html.toString());
-		StringBuilder javascript=new StringBuilder();
-		javascript.append("<script>");
-		
-		javascript.append("nv.addGraph(function() {");
-		javascript.append("var chart = nv.models.discreteBarChart()");
-		javascript.append(".x(function(d) { return d.label })");
-		javascript.append(".y(function(d) { return d.value })");
-		javascript.append(".staggerLabels(true)");
-		javascript.append(".tooltips(false)");
-		javascript.append(".showValues(true)");
-		
-		javascript.append("d3.select('#chart svg')");
-		javascript.append(".datum(data)");
-		javascript.append(".transition().duration(500)");
-		javascript.append(".call(chart)");
-		javascript.append(";");
-		
-		javascript.append("nv.utils.windowResize(chart.update);");
-		javascript.append("return chart;");
-		
-		javascript.append("});");
-		
+
+		StringBuffer javascript = new StringBuffer();
+		javascript.append("\n<script>");
+		javascript.append(json.toString());
+		javascript.append("\nnv.addGraph(function() {\n");
+		javascript.append(" var chart = nv.models.discreteBarChart()\n ");
+		javascript.append(".x(function(d) { return d.label })\n");
+		javascript.append(".y(function(d) { return d.value })\n");
+		javascript.append(".staggerLabels(true)\n");
+		javascript.append(".tooltips(false)\n");
+		javascript.append(".showValues(true) \n");
+
+		javascript.append("d3.select('#chart svg')\n");
+		javascript.append(".datum(exampleData())\n");
+		javascript.append(".transition().duration(500)\n");
+		javascript.append(".call(chart)\n");
+		javascript.append(";\n");
+
+		javascript.append(" nv.utils.windowResize(chart.update);\n");
+		javascript.append(" return chart;\n");
+
+		javascript.append(" });\n");
+
 		javascript.append("</script>");
 		writer.write(javascript.toString());
-		
+
 	}
 
 }
